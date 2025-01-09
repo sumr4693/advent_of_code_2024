8638ec3865e6bdf60aba5cc5638e9b421bf5cf09
sumr4693 <subramanian4693@gmail.com>
2025-01-09 20:25:43 +0100

    Day 7 part 2

--- day7.cpp
+++ day7.cpp
@@ -17,7 +17,8 @@ using namespace std;
 typedef enum {
     e_ADD,
     e_MULT,
-    e_NUM_OPERATIONS = 2
+    e_CONCATENATE,
+    e_NUM_OPERATIONS = 3
 } operators_t;
 
 template<typename T>
@@ -60,6 +61,10 @@ long long perform_math(long long a, long long b, operators_t op)
     case e_MULT:
         result = a * b;
         break;
+
+    case e_CONCATENATE:
+        result = stoll(to_string(a) + to_string(b));
+        break;
     
     default:
         break;
@@ -68,15 +73,15 @@ long long perform_math(long long a, long long b, operators_t op)
     return result;
 }
 
-long long perform_combination_of_ops(vector<long long> const v, int combination_num)
+long long perform_combination_of_ops(vector<long long> const v, int combination_num, int no_of_operators)
 {
     long long final_val = v[0];
     size_t v_size = v.size();
 
     for (int i = 1; i < v_size; i++)
     {
-        int op_type = combination_num % 2;
-        combination_num = floor(combination_num/2);
+        int op_type = combination_num % no_of_operators;
+        combination_num = floor(combination_num/no_of_operators);
 
         final_val = perform_math(final_val, v[i], (operators_t) op_type);
     }
@@ -94,9 +99,12 @@ int main()
 
     fileOperations<long long> fOp(file_path);
 
-    vector<long long> test_values;
-    vector<vector<long long>> op_values;
     vector<vector<long long>> equations;
+    vector<long long> test_values;
+    vector<long long> revisit_test_values;
+    vector<vector<long long>> op_values;
+    vector<vector<long long>> revisit_op_values;
+
     fOp.get_num_data_from_file(equations, ' ');
 
     for (int i = 0; i < equations.size(); i++)
@@ -122,15 +130,49 @@ int main()
 
         for (int c = 0; c < combinations; c++)
         {
-            if (test_values[i] == perform_combination_of_ops(op_values[i], c))
+            if (test_values[i] == perform_combination_of_ops(op_values[i], c, 2))
             {
                 part1_result += test_values[i];
                 break;
+            }
+            else if (c == combinations - 1)
+            {
+                revisit_test_values.push_back(test_values[i]);
+                revisit_op_values.push_back(op_values[i]);
+            }
+            else
+            {
+                // Do nothing
             }
         }
     }
 
     cout << "Part 1: " << part1_result << endl;
+
+    int num_revisit_lines = revisit_test_values.size();
+    long long part2_result = part1_result;
+
+    for (int i = 0; i < num_revisit_lines; i++)
+    {
+        int num_elements = revisit_op_values[i].size();
+        int operations = num_elements - 1;
+        int combinations = pow(3, operations);
+
+        for (int c = 0; c < combinations; c++)
+        {
+            if (revisit_test_values[i] == perform_combination_of_ops(revisit_op_values[i], c, 3))
+            {
+                part2_result += revisit_test_values[i];
+                break;
+            }
+            else
+            {
+                // Do nothing
+            }
+        }
+    }
+
+    cout << "Part 2: " << part2_result << endl;
 
     auto time_end = chrono::high_resolution_clock::now();
 
