3c3c7ebde217bbc2e00af6831d9648b21b84f226
sumr4693 <subramanian4693@gmail.com>
2025-02-08 17:25:31 +0100

    Day 12 Part 1 solution

--- day12.cpp
+++ day12.cpp
@@ -40,6 +40,32 @@ void print_vector(vector<vector<T>> const v)
     }
 }
 
+template<typename T>
+void print_vector(vector<pair<T,T>> const v)
+{
+    cout << "Printing elements in 1D vector: " << endl;
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << "(" << v[i].first << ", " << v[i].second << ") ";
+    }
+    cout << endl;
+}
+
+template<typename T>
+void print_vector(vector<vector<pair<T,T>>> const v)
+{
+    cout << "Printing elements in 2D vector: " << endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            cout << "(" << v[i][j].first << ", " << v[i][j].second << ") ";
+        }
+        cout << endl;
+    }
+}
+
 void print_map(map<char, vector<pair<int,int>>> myMap)
 {
     for(const auto& elem : myMap)
@@ -85,7 +111,7 @@ void find_garden_types(const vector<vector<char>>& garden_plots, map<char, vecto
             garden_types[garden_plots[i][j]].push_back(make_pair(i,j));
         }
     }
-    print_map(garden_types);
+    // print_map(garden_types);
 }
 
 void calculate_garden_perimeters(const vector<vector<char>>& garden_plots, vector<vector<int>>& garden_perimeters)
@@ -123,62 +149,97 @@ void calculate_garden_perimeters(const vector<vector<char>>& garden_plots, vecto
     }
 }
 
-long long calculate_fencing_price(const vector<vector<char>>& garden_plots, map<char, vector<pair<int,int>>> garden_types, const vector<vector<int>>& garden_perimeters)
+void find_garden_regions(const vector<vector<char>>& garden_plots, map<char, vector<pair<int,int>>> garden_types, vector<vector<pair<int,int>>>& garden_regions)
 {
-    long long fencing_price = 0;
-    size_t total_garden_types = garden_types.size();
+    pair<int,int> c;
+    map<pair<int,int>, bool> visited_history;
     size_t dimension = garden_plots.size();
-
-    map<char, vector<pair<int,int>>> fencing_price_map;
-
-    cout << "Type, Area, Perimeter: " << endl;
-
-    cell_t c;
 
     for (const auto& row : garden_types)
     {
-        int area = 0;
-        int perimeter = 0;
         size_t num_indices = row.second.size();
+        int visited_cells = 0;
 
-        if (num_indices > 0)
+        int i = 0;
+
+        while(visited_cells < num_indices)
         {
-            area++;
-            perimeter += garden_perimeters[row.second[0].first][row.second[0].second];
-        }
-        for (int i = 1; i < num_indices; i++)
-        {
-            directions_t dir = e_LEFT;
-            for (int k = 0; k < e_NUM_DIR; k++)
+            if (visited_history[row.second[i]] == false)
             {
-                c.row = row.second[i].first + dir_arr[dir][0];
-                c.col = row.second[i].second + dir_arr[dir][1];
+                vector<pair<int,int>> region_depth_graph;
+                vector<pair<int,int>> region_graph;
+                region_depth_graph.push_back(row.second[i]);
+                // if (garden_plots[row.second[i].first][row.second[i].second] == 'C')
+                // {
+                //     cout << endl;
+                //     cout << "(" << row.second[i].first << ", " << row.second[i].second << ")";
+                // }
 
-                if (!(c.row == -1 || c.row == dimension || c.col == -1 || c.col == dimension))
+                while (region_depth_graph.size() != 0)
                 {
-                    if (garden_plots[c.row][c.col] == row.first)
+                    region_graph.push_back(region_depth_graph.back());
+                    region_depth_graph.pop_back();
+                    pair<int,int> current_cell = region_graph.back();
+
+                    directions_t dir = e_LEFT;
+                    for (int k = 0; k < e_NUM_DIR; k++)
                     {
-                        area++;
-                        perimeter += garden_perimeters[row.second[i].first][row.second[i].second];
-                        break;
+                        c.first = current_cell.first + dir_arr[dir][0];
+                        c.second = current_cell.second + dir_arr[dir][1];
+
+                        if (!(c.first == -1 || c.first == dimension || c.second == -1 || c.second == dimension))
+                        {
+                            if (garden_plots[c.first][c.second] == row.first)
+                            {
+                                if ((find(region_graph.begin(), region_graph.end(), c) == region_graph.end()) &&
+                                    (find(region_depth_graph.begin(), region_depth_graph.end(), c) == region_depth_graph.end()))
+                                {
+                                    // if (garden_plots[c.first][c.second] == 'C')
+                                    // {
+                                    //     cout << "(" << c.first << ", " << c.second << ")";
+                                    // }
+                                    region_depth_graph.push_back(c);
+                                }
+                            }
+                        }
+                        dir = (directions_t) ((dir + 1) % e_NUM_DIR);
+                    }
+
+                    if (visited_history[current_cell] == false)
+                    {
+                        visited_cells++;
+                        visited_history[current_cell] = true;
                     }
                 }
-
-                if (k == 3)
-                {
-                    fencing_price += (area * perimeter);
-                    fencing_price_map[row.first].push_back(make_pair(area, perimeter));
-                    area = 0;
-                    perimeter = 0;
-                }
-                dir = (directions_t) ((dir + 1) % e_NUM_DIR);
+                garden_regions.push_back(region_graph);
+            }
+            else
+            {
+                i = (i + 1) % num_indices;
             }
         }
-        fencing_price += (area * perimeter);
-        fencing_price_map[row.first].push_back(make_pair(area, perimeter));
     }
+    // cout << endl;
+    // print_vector<int>(garden_regions);
+}
 
-    print_map(fencing_price_map);
+long long calculate_fencing_price(const vector<vector<pair<int,int>>>& garden_regions, const vector<vector<int>>& garden_perimeters)
+{
+    long long fencing_price = 0;
+    size_t total_garden_regions = garden_regions.size();
+
+    for (int i = 0; i < total_garden_regions; i++)
+    {
+        int area = garden_regions[i].size();
+        int perimeter = 0;
+
+        for (int j = 0; j < area; j++)
+        {
+            perimeter += garden_perimeters[garden_regions[i][j].first][garden_regions[i][j].second];
+        }
+
+        fencing_price += (area * perimeter);
+    }
 
     return fencing_price;
 }
@@ -197,6 +258,7 @@ int main()
     vector<vector<char>> garden_plots;
     map<char, vector<pair<int,int>>> garden_types;
     map<char, int> garden_areas;
+    vector<vector<pair<int,int>>> garden_regions;
     long long fencing_price = 0;
 
     fOp.get_char_data_from_file(garden_plots);
@@ -207,7 +269,8 @@ int main()
     vector<vector<int>> garden_perimeters(dimension, vector<int>(dimension, 0));
     calculate_garden_perimeters(garden_plots, garden_perimeters);
 
-    fencing_price = calculate_fencing_price(garden_plots, garden_types, garden_perimeters);
+    find_garden_regions(garden_plots, garden_types, garden_regions);
+    fencing_price = calculate_fencing_price(garden_regions, garden_perimeters);
 
     cout << "Fencing price, part 1: " << fencing_price << endl;
 
